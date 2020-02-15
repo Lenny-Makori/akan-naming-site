3f1b647184d251c61ec02d599734c86bd13b4a6d
Lenny Makori <makorilenny28@gmail.com>
2020-02-15 21:45:27 +0300

    Initialize array of male and female Akan names

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Initialize array of male and female Akan names
+const maleAkanNames = [
+  "Kwame",
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi"
+];
+const femaleAkanNames = [
+  "Ama",
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua"
+];
