654fe9edccea9ca7964805b355b2dde641f24df2
Lenny Makori <makorilenny28@gmail.com>
2020-02-15 21:47:43 +0300

    Initialize instances to collect form data

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,16 @@ const femaleAkanNames = [
   "Yaa",
   "Afua"
 ];
+// Initialize instances to collect form data
+function fetchFormData() {
+  let form = document.forms[0];
+  let day, month, year, gender;
+
+  gender = form.gender.value;
+
+  day = parseInt(form.day.value);
+  month = parseInt(form.month.value);
+  year = parseInt(form.year.value);
+
+  return [gender, day, month, year];
+}
