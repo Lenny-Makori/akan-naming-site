b6c2cf498bbe83b96d212e9e3903c71c877ddd39
Lenny Makori <makorilenny28@gmail.com>
2020-02-17 04:10:01 +0300

    Modify the function to work correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 // Initialize array of male and female Akan names
 const maleAkanNames = [
-  "Kwame",
   "Kwasi",
   "Kwadwo",
   "Kwabena",
   "Kwaku",
   "Yaw",
-  "Kofi"
+  "Kofi",
+  "Kwame"
 ];
 const femaleAkanNames = [
-  "Ama",
   "Akosua",
   "Adwoa",
   "Abenaa",
   "Akua",
   "Yaa",
-  "Afua"
+  "Afua",
+  "Ama"
 ];
 // Initialize instances to collect form data
-function fetchFormData() {
+function collectUserInfo() {
   let form = document.forms[0];
   let day, month, year, gender;
 
@@ -30,29 +30,36 @@ function fetchFormData() {
 
   return [gender, day, month, year];
 }
-function calculateWeekDay(birthDate) {
+// Initialize instances to calculate the day of the week
+function calculateDayOfWeek(birthDate) {
   let [dayOfMonth, monthOfYear, yearOfBirth] = birthDate;
-  let zeroBasedCentury, yearOfCentury;
+  let centuryDigits, yearDigits;
 
 
-  if (monthOfYear <= 0) {
+  if (monthOfYear < 1) {
     monthOfYear += 12;
     yearOfBirth -= 1;
   }
 
 
-  zeroBasedCentury = parseInt(yearOfBirth / 100);
-  yearOfCentury = yearOfBirth % 100;
+  centuryDigits= parseInt(yearOfBirth/100);
+  yearDigits=parseInt(yearOfBirth%100);
 
-  let dayOfWeek =
-    (dayOfMonth +
-      parseInt(
-        2.6 * (monthOfYear + 1) +
-          yearOfCentury +
-          parseInt(yearOfCentury / 4) +
-          parseInt(zeroBasedCentury / 4) +
-          5 * zeroBasedCentury
-      )) %
-    7;
+  let dayOfWeek = (parseInt(((centuryDigits/4)-2*centuryDigits-1)+(5*parseInt(yearDigits/4))+(parseInt(26*(monthOfYear + 1)/10 +dayOfMonth)))%7)-1;
   return dayOfWeek;
+}
+// This function will fetch the Akan name for the day of the week and specified gender
+function findAkanName() {
+  let formData = collectUserInfo();
+  let userBirthDate, userGender, dayOfWeek;
+
+  [userGender, ...userBirthDate] = formData;
+  dayOfWeek = calculateDayOfWeek(userBirthDate);
+
+  if (userGender === "Male") {
+    alert("Your Akan Name is: "+maleAkanNames[dayOfWeek]);
+  } else {
+    alert("Your Akan Name is: "+femaleAkanNames[dayOfWeek]);
+  }
+  return false;
 }
